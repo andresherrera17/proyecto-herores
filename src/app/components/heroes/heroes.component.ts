edf39428a72049df91c2977080c28d6b33b80fd7
Andres Herrera <lherrera>
2021-11-19 18:55:31 -0500

    observable heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -11,10 +11,20 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
+  heroe: Heroe = {
+    nombre: "Daredevil",
+    bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
+    img: "assets/img/daredevil.png",
+    aparicion: "1964-01-01",
+    casa: "Marvel"
+  }
+
   constructor(private _heroesServices: HeroesService) { }
 
   ngOnInit(): void {
-    this.heroes = this._heroesServices.getHeroes();
+    this._heroesServices.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+    });
   }
 
 
