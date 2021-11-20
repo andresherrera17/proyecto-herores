d64bc4d6394e3aaae3da0de337cd9a8ea521984f
Andres Herrera <lherrera>
2021-11-20 11:18:19 -0500

    select pais

--- src/app/components/formularios/formulario-ngmodel/formulario-ngmodel.component.ts
+++ src/app/components/formularios/formulario-ngmodel/formulario-ngmodel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { IPais } from 'src/app/interfaces/pais.interface';
 import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
@@ -9,18 +10,25 @@ import { PaisService } from 'src/app/services/pais.service';
 })
 export class FormularioNgmodelComponent implements OnInit {
 
-  nombre: string = "";
+  pais: any[] = [];
+  valorPais: string;
 
   constructor(private _paisService: PaisService) { }
 
   ngOnInit(): void {
     this._paisService.getPaises().subscribe(paises => {
-      console.log(paises);
+      this.pais = paises;
+
+      this.pais.unshift({
+        nombre: '[Selecciones País]',
+        codigo: ''
+      })
     })
   }
 
   guardar(form: NgForm) {
     console.log(form);
+    console.log(this.valorPais);
   }
 
 }
