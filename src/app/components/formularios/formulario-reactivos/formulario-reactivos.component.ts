0e8ea54be6db2758aa44614ef00f4cd74f9159dd
Andres Herrera <lherrera>
2021-11-22 18:54:41 -0500

    crear formulario ts

--- src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
+++ src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivos',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioReactivosComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  buildForm() {
+    const hoy = new Date().toISOString().substring(0, 10);
+    const name = "Andres Herrera";
+
+    this.formGroup = this.formBuilder.group({
+      fechaCreacion: hoy,
+      nombre: name,
+      email: 'andresherrerab97@gmail.com',
+      contrasena: ''
+    });
   }
 
 }
