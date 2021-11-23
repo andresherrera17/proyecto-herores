cad43b216aaf51966b44922549c3e247cff12deb
Andres Herrera <lherrera>
2021-11-22 19:38:07 -0500

    formulario mas errores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './components/about/about.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { EjemplosPipesComponent } from './components/ejemplos-pipes/ejemplos-pipes.component';
 import { FormularioNgmodelComponent } from './components/formularios/formulario-ngmodel/formulario-ngmodel.component';
+import { FormularioReactivosComponent } from './components/formularios/formulario-reactivos/formulario-reactivos.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HomeComponent } from './components/home/home.component';
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: 'buscar/:termino', component: BuscadorComponent },
   { path: 'pipes', component: EjemplosPipesComponent },
   { path: 'form1', component: FormularioNgmodelComponent },
+  { path: 'form2', component: FormularioReactivosComponent },
   { path: '**', pathMatch: 'full', redirectTo: "home" },
 ];
 
