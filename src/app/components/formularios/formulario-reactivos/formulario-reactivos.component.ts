4d6b80e7143af3005b2aa6e9a6b8ccb5d66b5fc8
Andres Herrera <lherrera>
2021-11-22 19:37:58 -0500

    formulario mas errores

--- src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
+++ src/app/components/formularios/formulario-reactivos/formulario-reactivos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivos',
@@ -25,10 +25,42 @@ export class FormularioReactivosComponent implements OnInit {
 
     this.formGroup = this.formBuilder.group({
       fechaCreacion: hoy,
-      nombre: name,
-      email: 'andresherrerab97@gmail.com',
-      contrasena: ''
+      nombre: [name, Validators.required],
+      email: ['andresherrerab97@gmail.com', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required, Validators.minLength(5), this.validarContrasena]],
+
     });
   }
+
+  validarContrasena(control: AbstractControl) {
+    const contrasena = control.value;
+    let error = null;
+
+    if (!contrasena.includes('$')) {
+      error = { pesos: 'Es requerido el sigo $' }
+    }
+    if (!parseFloat(contrasena[0])) {
+      error = { ...error, number: 'Debe empezar con un numero' }
+    }
+
+    return error;
+  }
+
+  getError(controlName: string) {
+    let error = '';
+    const control = this.formGroup.get(controlName);
+    if (control.touched && control.errors != null) {
+      error = JSON.stringify(control.errors);
+    }
+    return error;
+  }
+
+
+
+  save() {
+    console.log(this.formGroup);
+  }
+
+
 
 }
